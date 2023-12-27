0eab978b233e5b3a3a4a675c095dbc81dacf57c7
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-27 16:42:54 +0530

    bucket index implemented

--- src/main/java/com/example/HashMap/HashMap.java
+++ src/main/java/com/example/HashMap/HashMap.java
@@ -1,14 +1,19 @@
 package com.example.HashMap;
 
 public class HashMap {
-    private LinkedList list;
+    private static final int CAPACITY = 20;
+    private LinkedList[] buckets;
 
     public HashMap() {
-        this.list = new LinkedList();
+        this.buckets = new LinkedList[CAPACITY];
+
+        for (int i = 0; i < CAPACITY; i++) {
+            this.buckets[i] = new LinkedList();
+        }
     }
 
     public HashMap(String sentence) {
-        this.list = new LinkedList();
+        this();
 
         String[] words = sentence.split(" ");
 
@@ -20,6 +25,23 @@ public class HashMap {
     public void add(String data) {
         data = data.toLowerCase();
 
+        int index = generateHashCode(data);
+        addInList(buckets[index], data);
+
+    }
+
+    public int get(String data) {
+        data = data.toLowerCase();
+
+        int index = generateHashCode(data);
+        return getFromList(buckets[index], data);
+    }
+
+    private int generateHashCode(String data) {
+        return Math.abs(data.hashCode()) % CAPACITY;
+    }
+
+    private void addInList(LinkedList list, String data) {
         Node current = list.head;
         while (current != null) {
             if (current.key.equals(data)) {
@@ -32,9 +54,7 @@ public class HashMap {
         list.add(data);
     }
 
-    public int get(String data) {
-        data = data.toLowerCase();
-
+    private int getFromList(LinkedList list, String data) {
         Node current = list.head;
         while (current != null) {
             if (current.key.equals(data)) {
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,10 +4,12 @@ import com.example.HashMap.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap map = new HashMap("To be or not to be");
-        System.out.println("Frequency of to: " + map.get("to"));
+        HashMap map = new HashMap(
+                "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations");
 
-        map.add("TO");
-        System.out.println("Frequency of to after adding another to: " + map.get("TO"));
+        System.out.println("Frequency of they: " + map.get("they"));
+
+        map.add("They");
+        System.out.println("Frequency of they after adding another they: " + map.get("they"));
     }
 }
